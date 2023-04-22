8bf1366c7ceb90dc8a872a615b2ae62951186f29
Brandon <73447772+CodingUnderPressure@users.noreply.github.com>
2023-04-21 20:38:05 -0400

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+
+    cars = {'Audi': 50000,
+            'Ford': 30000,
+            'BMW': 60000,
+            'Toyota': 35000}
+
+    return render_template('index.html', cars = cars)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
