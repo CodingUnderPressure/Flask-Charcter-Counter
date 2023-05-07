3f1c24168e0a669f367ce4a960aa041359329050
Brandon <73447772+CodingUnderPressure@users.noreply.github.com>
2023-05-07 12:34:15 -0400

    add bootstrap

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def test():
 
     return render_template('index.html', cars = cars)
 
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
